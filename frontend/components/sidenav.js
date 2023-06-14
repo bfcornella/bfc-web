859cf6fd2a212873a03d547011f6fd42271c0754
bfcornella <134495385+bfcornella@users.noreply.github.com>
2023-06-14 22:54:37 +0000

    Fix submenu

--- frontend/components/sidenav.js
+++ frontend/components/sidenav.js
@@ -12,7 +12,22 @@ const SideNav = ({ categories }) => {
                   <Link className="uk-link-reset" href={category.attributes.slug == 'home'? `/`:`/category/${category.attributes.slug}`}>
                     {category.attributes.name}
                   </Link>
+                  {category.attributes.subcategories.data.length>0 &&
+                      <div className="uk-navbar-dropdown-black">
+                          <ul className="uk-nav uk-navbar-dropdown-nav">
+                            {category.attributes.subcategories.data.map((subcategory) => {
+                              return (
+                                <li key={subcategory.attributes.id}>
+                                  <Link className="uk-link-reset"  style={{fontSize: 16}} href={category.attributes.slug == 'home'? `/`:`/subcategory/${subcategory.attributes.slug}`}>
+                                    {subcategory.attributes.name}
+                                  </Link>
                                 </li>
+                              );
+                            })}
+                          </ul>
+                        </div>
+                    }
+                </li>
               );
             })}
         </ul>
