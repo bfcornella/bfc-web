451afcf5d8cc0047ea60712a09ed6af640e6fd86
Marta González <marta.gonzalez@litesolutions.es>
2023-06-16 16:16:40 +0200

    Campus y ultimas noticias

--- frontend/components/categories/campus.js
+++ frontend/components/categories/campus.js
@@ -8,26 +8,38 @@ const Campus = ({ category, articles }) => {
           <div class="uk-height-small uk-flex uk-flex-middle">
             <h1><span class="uk-text-muted"> {category.attributes.name}</span></h1>
         </div>
-       
-        {articles.map((article, i) => {
-                  return (
-                        <div className="uk-container uk-width-1-1">
-                        <div uk-grid className="uk-child-width-expand@s">
-                            <div className="uk-width-1-3@m uk-align-left uk-margin-remove"> 
-                              <div class="uk-margin uk-card uk-card-default uk-card-body">
-                                <div className="uk-align-center" style={{width: 350}}>
-                                <img src={getStrapiMedia(article.attributes.cover)} width="500" height="500" alt=""/>
-                                </div>
-                                <div class="uk-text-center" style={{padding: 15}}>
-                                  <span dangerouslySetInnerHTML={{__html:article.attributes.description}}></span>
-                                </div>
-                              </div>
-                            </div>
-                            </div>
-                          </div>
-        );
-      })}
-
+        
+       <div  className=" uk-visible@s">
+          <div className="uk-align-left uk-margin-remove"> 
+              <div class="uk-text-left" style={{padding: 15}} >
+                      <span dangerouslySetInnerHTML={{__html:category.attributes.description}}></span>
+              </div>
+              <div class="uk-text-center" style={{padding: 15}} >
+                  <iframe
+                          src="/images/Campus_bfc_2023.pdf"
+                          title="Async & Performance"
+                          width="800"
+                          height="600"
+                        />
+              </div>
+          </div>  
+       </div>
+       <div  className="uk-width-auto uk-hidden@s uk-text-muted">
+                <div className="uk-align-left uk-margin-remove"> 
+                    <div class="uk-text-left" style={{padding: 15}} >
+                            <span dangerouslySetInnerHTML={{__html:category.attributes.description}}></span>
+                    </div>
+                    <div class="uk-text-center" style={{padding: 15}} >
+                    <a
+                          style={{color: '#daa99a', fontWeight: 600}}
+                          href="/images/Campus_bfc_2023.pdf"
+                          alt="Descarregar informació campus"
+                          target="_blank"
+                        >Descarregar informació campus</a>
+                      </div>
+                </div>  
+       </div>
+        
     </div>
 
 
