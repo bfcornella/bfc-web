adb3ca8149af2d7f73867144330ae6c9b915e382
Marta González <marta.gonzalez@litesolutions.es>
2023-06-16 19:11:42 +0200

    Added gpc sponsor

--- frontend/components/footerApp.js
+++ frontend/components/footerApp.js
@@ -7,48 +7,55 @@ const FooterApp = ({ global }) => {
     <footer className="uk-background-primary">
       <div className="uk-container uk-background-light-primary uk-width-1-1">
       <div uk-grid className="uk-grid-small uk-child-width-expand@s uk-text-small">
-          <div className="uk-width-1-3@m uk-align-left uk-margin-remove"> 
-              <div className="uk-grid">
-                <div className="uk-width-1-3@m uk-align-center uk-padding-small"> 
-                  <Image src="/AF_Tram_NegColor.jpg"
-                    width={100}
-                    height={100}
-                    alt="Home"
-                  />
-                </div>
-                <div className="uk-width-1-3@m uk-align-center uk-padding-small">
+          <div className="uk-width-1-3@m uk-align-left uk-margin-remove">
+              <div className="uk-grid uk-margin-remove">
+              <div className="uk-width-3-3@m uk-align-center">
                   <Image src="/ajcornella_color_negatiu_cen.png"
                     width={180}
                     height={180}
-                    alt="Home"
+                    alt="Ajuntament de Cornellà de Llobregat"
                   />
                 </div>
-                <div className="uk-width-1-3@m uk-align-center uk-padding-small"> 
-                  <Image src="/logo-montserrat.svg"
-                    width={150}
-                    height={150}
-                    alt="Home"
+                <div className="uk-width-1-3@m uk-align-center"> 
+                  <Image src="/AF_Tram_NegColor.jpg"
+                    width={100}
+                    height={100}
+                    alt="TRAM"
                   />
                 </div>
-                <div className="uk-width-1-3@m uk-align-center uk-padding-small"> 
-                  <Image src="/logo_clinica_dental_eucaliptus.png"
-                    width={120}
-                    height={120}
-                    alt="Home"
-                  />
-                </div>
-                <div className="uk-width-1-3@m uk-align-center uk-padding-small"> 
-                  <Image src="/logo_esports_cornella_sense_fons.png"
-                    width={150}
-                    height={150}
-                    alt="Home"
-                  />
-                </div>
-                <div className="uk-width-1-3@m uk-align-center uk-padding-small"> 
+                <div className="uk-width-1-3@m uk-align-center"> 
                     <Image src="/megagame.png"
                     width={150}
                     height={150}
-                    alt="Home"
+                    alt="Megagame"
+                  />
+                </div>
+                <div className="uk-width-1-3@m uk-align-center"> 
+                  <Image src="/logo_clinica_dental_eucaliptus.png"
+                    width={120}
+                    height={120}
+                    alt="Clínica dental Eucaliptus"
+                  />
+                </div>
+                <div className="uk-width-1-3@m uk-align-center"> 
+                  <Image src="/logo_esports_cornella_sense_fons.png"
+                    width={150}
+                    height={150}
+                    alt="Esports Cornellà"
+                  />
+                </div>
+                <div className="uk-width-1-3@m uk-align-center"> 
+                  <Image src="/logo-montserrat.svg"
+                    width={150}
+                    height={150}
+                    alt="Montserrat"
+                  />
+                </div>
+                <div className="uk-width-1-3@m uk-align-center"> 
+                    <Image src="/gpc.jpg"
+                    width={150}
+                    height={150}
+                    alt="GPC"
                   />
                 </div>
               </div>
