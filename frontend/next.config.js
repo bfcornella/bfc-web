8b6047f5b48e571994125b155ae1e204bb049e7f
Albert <albertsanchom@gencat.cat>
2023-10-07 17:15:39 +0200

    Setup new bfc strapi instance on fly.io

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: "default",
-    domains: ["localhost", "127.0.0.1", "bfcornella-fluffy-telegram-9p4v5q6qv6w3x679-1337.preview.app.github.dev", "bfc-backend.fly.dev", "res.cloudinary.com"],
+    domains: ["localhost", "127.0.0.1", "bfcornella-fluffy-telegram-9p4v5q6qv6w3x679-1337.preview.app.github.dev", "bfc-cms.fly.dev", "res.cloudinary.com"],
   }
 }
 
