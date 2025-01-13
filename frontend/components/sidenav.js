b6094c19f92c163b9b643dc223d33c3f05ae4c69
Create2Tech <create2tech@gmail.com>
2025-01-13 11:50:15 +0100

    Sidenav buttons

--- frontend/components/sidenav.js
+++ frontend/components/sidenav.js
@@ -1,49 +1,62 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 
 const SideNav = ({ categories }) => {
   return (
-<div id="sidenav" navbar-toggle-icon="true" uk-offcanvas="flip: true" className="uk-offcanvas">
-
-    <div className="uk-offcanvas-bar">
-   
-        <ul className="uk-nav">
-        {categories.map((category) => {
-              return (
-                <li key={category.id} >
-                 
-                  {category.attributes.subcategories.data.length==0 &&
-                     <Link className="uk-link-reset" href={category.attributes.slug == 'home'? `/`:`/category/${category.attributes.slug}`}>
-                      {category.attributes.name}
-                    </Link>
-                    }
-                  {category.attributes.subcategories.data.length>0 &&
-                    <Link className="uk-link-reset uk-parent" href="#">
-                      {category.attributes.name}
-                    </Link>
-                  }
-                  {category.attributes.subcategories.data.length>0 &&
-                      
-                           <ul class="uk-nav-sub">
-                            {category.attributes.subcategories.data.map((subcategory) => {
-                              return (
-                                <li key={subcategory.attributes.id}>
-                                  <Link className="uk-link-reset"  style={{fontSize: 16}} href={category.attributes.slug == 'home'? `/`:`/subcategory/${subcategory.attributes.slug}`}>
-                                    {subcategory.attributes.name}
-                                  </Link>
-                                </li>
-                              );
-                            })}
-                          </ul>
-                       
-                    }
-                </li>
-              );
-            })}
+    <div id='sidenav' navbar-toggle-icon='true' uk-offcanvas='flip: true' className='uk-offcanvas'>
+      <div className='uk-offcanvas-bar'>
+        <ul className='uk-nav'>
+          {categories.map((category) => {
+            return (
+              <li key={category.id}>
+                {category.attributes.subcategories.data.length == 0 && (
+                  <Link
+                    className='uk-link-reset'
+                    href={category.attributes.slug == 'home' ? `/` : `/category/${category.attributes.slug}`}
+                  >
+                    {category.attributes.name}
+                  </Link>
+                )}
+                {category.attributes.subcategories.data.length > 0 && (
+                  <Link className='uk-link-reset uk-parent' href='#'>
+                    {category.attributes.name}
+                  </Link>
+                )}
+                {category.attributes.subcategories.data.length > 0 && (
+                  <ul class='uk-nav-sub'>
+                    {category.attributes.subcategories.data.map((subcategory) => {
+                      return (
+                        <li key={subcategory.attributes.id}>
+                          <Link
+                            className='uk-link-reset'
+                            style={{ fontSize: 16 }}
+                            href={
+                              category.attributes.slug == 'home' ? `/` : `/subcategory/${subcategory.attributes.slug}`
+                            }
+                          >
+                            {subcategory.attributes.name}
+                          </Link>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                )}
+              </li>
+            );
+          })}
+          <li>
+            <Link className='uk-link-reset' href='https://www.basquetcatala.cat/club/192'>
+              Federació Catalana BQ
+            </Link>
+          </li>
+          <li>
+            <Link className='uk-link-reset' href='https://app.basquetfemenicornella.com'>
+              Accés APP
+            </Link>
+          </li>
         </ul>
+      </div>
     </div>
-  
-</div>
   );
 };
 
